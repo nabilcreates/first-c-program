a143d3c06495e74c5faaee892e7518599585e335
Nabil Ridhwanshah <nabilrdhwn@gmail.com>
2019-06-20 21:17:57 +0800

    Simulating server uptime logging

--- main.c
+++ main.c
@@ -1,12 +1,57 @@
+// This program simulates a server-uptime log, it will reset (or refresh) every 5ms and everytime it resets, the reset count will increase. But if it reaches the maximum amount of times it can reset, the server will stop.
+
 #include <stdio.h>
+#include <stdlib.h>
+
+// Creates a structure
+struct serverstatus
+{
+    long uptime;
+    long reset_count;
+};
+
+// Function to print values (attempt to declutter code below)
+void printValues(long uptime, long reset_count)
+{
+    // Printf is formatted printing (like in python)
+    // %li prints long ints and \n is an line escape
+    printf("Server Uptime: %li \n", uptime);
+    printf("Server Reset Count: %li \n\n", reset_count);
+}
+
 int main()
 {
-    int a,b,final;
+    // Define structure as server_one
+    struct serverstatus server_one;
 
-    // Set the value to variables
-    a = 10, b = 5, final = a + b;
+    // Variables in the use
+    long uptime = 0;
+    long resetcount = 0;
+    int max_reset_count = 20;
+    
+    // If the reset count is below the max reset count, then loop
+    do
+    {
+        // If the uptime if less than 5, then loop
+        do
+        {
+            // Increments the uptime
+            server_one.uptime = uptime++;
 
-    // Print final variable
-    printf("myvar: %i", final);
-    return 0;
+            // If the uptime is above or equal to 5
+            if(server_one.uptime >= 5)
+            {
+                // Increment the reset count
+                server_one.reset_count = resetcount ++;
+
+                // Uptime will be resetted to 0
+                uptime = 0;
+            }
+
+            // Run the printValues function
+            printValues(server_one.uptime, server_one.reset_count);
+            
+        } while(server_one.uptime < 5);
+
+    } while(server_one.reset_count < max_reset_count);
 }
