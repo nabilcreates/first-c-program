15b5138f6c3c63f1dc68d53786bdf3699d12e250
Nabil Ridhwanshah <nabilrdhwn@gmail.com>
2019-06-20 19:38:13 +0800

    First C Program!!!

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    int a,b,final;
+
+    // Set the value to variables
+    a = 10, b = 5, final = a + b;
+
+    // Print final variable
+    printf("myvar: %i", final);
+    return 0;
+}
